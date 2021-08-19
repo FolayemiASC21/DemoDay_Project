c02ef8af0a8cff581b954eb7a719ed17301a453b
FolayemiASC21 <87777050+FolayemiASC21@users.noreply.github.com>
2021-08-19 14:02:45 -0400

    first commit

---
+++ database.js
@@ -0,0 +1,177 @@
+let  database = {
+    "Agriculture, forestry, fishing, and hunting": {
+        1: {
+            "info": {
+                "name":"Crop production",
+                "description":"Agricultural workers maintain crops and tend livestock.",
+                "total":"1,186",
+                "BlackEmployed":"2.8%",
+                "AsianEmployed":"1.2%",
+                "HispanicEmployed":"24.2%",
+            },
+        },
+    
+        2: {
+            "info": {
+                "name":"Animal production and aquaculture",
+                "total":"832,000",
+                "BlackEmployed":"3.3%",
+                "AsianEmployed":"0.4%",
+                "HispanicEmployed":"17.9%",
+            },
+        },
+    
+        3: {
+            "info": {
+                "name":"Support activities for agriculture and forestry",
+                "total":"145,000",
+                "BlackEmployed":"5.7%",
+                "AsianEmployed":"0.4%",
+                "HispanicEmployed":"26%",
+                
+            },
+        },
+    
+        4: {
+            "info": {
+                "name":"Forestry (except logging)",
+                "total":"53,000",
+                "BlackEmployed":"0.0%",
+                "AsianEmployed":"0.5%",
+                "HispanicEmployed":"9.9%",
+                
+            },
+    
+        },
+    },
+    
+    "Mining, quarrying, and oil and gas extraction": {
+        1: {
+            "info": {
+                "name":"Oil and gas extraction",
+                "total":"97,000",
+                "BlackEmployed":"4,1%",
+                "AsianEmployed":"10.5%",
+                "HispanicEmployed":"13%",
+            }
+        },
+    
+        2: {
+            "info": {
+                "name":"Coal Mining",
+                "total":"51,000",
+                "BlackEmployed":"3.1%",
+                "AsianEmployed":"0.0%",
+                "HispanicEmployed":"4.2",
+            }
+        },
+    
+        3: {
+            "info": {
+                "name":"Nonmetallic mineral mining and quarrying",
+                "total":"83,000",
+                "BlackEmployed":"0.8%",
+                "AsianEmployed":"2.5%",
+                "HispanicEmployed":"12.7",
+            }
+        },
+    
+        4: {
+            "info": {
+                "name":"Support activities for mining",
+                "total":"417,000",
+                "BlackEmployed":"6.0%",
+                "AsianEmployed":"3.9%",
+                "HispanicEmployed":"20.7",
+    
+            },
+        },
+    
+    },
+    
+    "Manufacturing":{
+        1: {
+            "info":{
+                "name":"Durable goods manufacturing",
+                "total":"9,129",
+                "BlackEmployed":"9.5%",
+                "AsianEmployed":"7.4%",
+                "HispanicEmployed":"15.5%",
+            },
+        },
+    
+        2: {
+            "info":{
+                "name":"Nonmetallic mineral products manufacturing",
+                "total":"410,000",
+                "BlackEmployed":"8.8%",
+                "AsianEmployed":"2.4%",
+                "HispanicEmployed":"24.4%",
+            },
+        },
+    
+        3: {
+            "info":{
+                "name":"Glass and glass product manufacturing",
+                "total":"123,000",
+                "BlackEmployed":"10.6%",
+                "AsianEmployed":"4.3%",
+                "HispanicEmployed":"21.8%",
+            },
+        },
+    
+        4: {
+            "info":{
+                "name":"Cement, concrete, lime, and gypsum product manufacturing",
+                "total":"157,000",
+                "BlackEmployed":"7.1%",
+                "AsianEmployed":"2.1%",
+                "HispanicEmployed":"23.1%",
+            },
+        },
+    },
+    
+    "Wholesale and retail trade": {
+        1: {
+            "info":{
+                "name":"Motor vehicle and motor vehicle parts and supplies merchant wholesalers",
+                "total":"151,000",
+                "BlackEmployed":"8.6%",
+                "AsianEmployed":"4.4%",
+                "HispanicEmployed":"10.4%",
+            },
+        },
+    
+        2: {
+            "info":{
+                "name":"Furniture and home furnishing merchant wholesalers",
+                "total":"58,000",
+                "BlackEmployed":"3.2%",
+                "AsianEmployed":"6.3%",
+                "HispanicEmployed":"19.7%",
+            },
+        },
+    
+        3: {
+            "info":{
+                "name":"Lumber and other construction materials merchant wholesalers",
+                "total":"151,000",
+                "BlackEmployed":"5.9%",
+                "AsianEmployed":"0.9%",
+                "HispanicEmployed":"23.8%",
+            },
+        },
+    
+        4: {
+            "info":{
+                "name":"Professional and commercial equipment and supplies merchant wholesalers",
+                "total":"317,000",
+                "BlackEmployed":"10.4%",
+                "AsianEmployed":"5.2%",
+                "HispanicEmployed":"11.8%",
+            }, 
+        },
+    }
+    
+    
+    }
---
+++ script.js
@@ -0,0 +1,180 @@
+let button1 = document.querySelector("#button1")
+
+let content1 = document.querySelector("#content1")
+
+let AFFH = "Agriculture, forestry, fishing, and hunting"
+let MQOGE = "Mining, quarrying, and oil and gas extraction"
+
+//content 1
+for (let x in database[AFFH]) {
+    let text = database[AFFH][x]
+
+    let contentPart = document.createElement("a")
+        contentPart.innerHTML = database[AFFH][x].info.name
+        contentPart.id = database[AFFH][x].info.name.toString()
+        contentPart.className = "AFFH"
+        contentPart.href = "#"
+        content1.appendChild(contentPart)
+}
+//end of content 1
+
+//content 2
+for (let x in database[MQOGE]) {
+    let text = database[MQOGE][x]
+
+    let contentPart = document.createElement("a")
+        contentPart.innerHTML = database[MQOGE][x].info.name
+        contentPart.id = database[MQOGE][x].info.name.toString()
+        contentPart.className = "MQOGE"
+        contentPart.href = "#"
+        content2.appendChild(contentPart)
+}
+//end of content 2
+// ^^^^^ Also Navbar stuff do not touch ^^^^^
+
+for (let i = 1; i < 5; i++) {
+    let Id = database[AFFH][i].info.name
+document.getElementById(Id).onclick = GFG_click;
+
+}
+
+for (let i = 1; i < 5; i++) {
+
+    let Id2 = database[MQOGE][i].info.name
+    document.getElementById(Id2).onclick = GFG_click;
+}
+
+
+function GFG_click(clicked) {
+    clickedButton = this.id
+    console.log(clickedButton)
+
+    profiles.innerHTML = ""
+
+    for (let i = 1; i < 5; i++) {
+
+        //dropdown 1
+        if (clickedButton == database[AFFH][i].info.name) {
+
+            let Header = document.createElement("h1")
+            let div = document.createElement("div")
+            // let image = document.createElement("img")
+            let text1 = document.createElement("p")
+            let text2 = document.createElement("p")
+            let text3 = document.createElement("p")
+            let text4 = document.createElement("p")
+
+            // image.src
+            Header.innerHTML = database[AFFH][i].info.name
+            text1.innerHTML = "What do people do in the " + database[AFFH][i].info.name + " industry?: "
+            text2.innerHTML = database[AFFH][i].info.description + "<hr>"
+            text3.innerHTML = "The total amount of people who work in this industry is about " + database[AFFH][i].info.total
+            text4.innerHTML = "Out of the " + database[AFFH][i].info.total + " people who work in this industry " + database[AFFH][i].info.BlackEmployed + " are Black/African American, " + database[AFFH][i].info.AsianEmployed + " are Asian and, " + database[AFFH][i].info.HispanicEmployed + " are Hispanic/Latino."
+
+            // div.appendChild(image)
+            div.appendChild(Header)
+            div.appendChild(text1)
+            div.appendChild(text2)
+            div.appendChild(text3)
+            div.appendChild(text4)
+            profiles.appendChild(div)
+        }
+        //end of dropdown 1
+
+        //drowpdown 2
+        else if (clickedButton == database[MQOGE][i].info.name) {
+
+            let Header = document.createElement("h1")
+            let div = document.createElement("div")
+            // let image = document.createElement("img")
+            let text1 = document.createElement("p")
+            let text2 = document.createElement("p")
+            let text3 = document.createElement("p")
+            let text4 = document.createElement("p")
+
+            // image.src
+            Header.innerHTML = database[MQOGE][i].info.name
+            text1.innerHTML = "What do people do in the " + database[MQOGE][i].info.name + " industry?: "
+            text2.innerHTML = database[MQOGE][i].info.description + "<hr>"
+            text3.innerHTML = "The total amount of people who work in this industry is about " + database[MQOGE][i].info.total
+            text4.innerHTML = "Out of the " + database[MQOGE][i].info.total + " people who work in this industry " + database[MQOGE][i].info.BlackEmployed + " are Black/African American, " + database[MQOGE][i].info.AsianEmployed + " are Asian and, " + database[MQOGE][i].info.HispanicEmployed + " are Hispanic/Latino."
+
+            // div.appendChild(image)
+            div.appendChild(Header)
+            div.appendChild(text1)
+            div.appendChild(text2)
+            div.appendChild(text3)
+            div.appendChild(text4)
+            profiles.appendChild(div)
+        }
+        //end of dropdown 2
+        
+    }
+}
+
+
+// VVVVVV Navbar stuff do not touch VVVVVV
+
+//button 1
+button1.onmouseover = function(event) {
+    event.preventDefault()
+
+    button1.style.backgroundColor = "red"
+    content1.style.backgroundColor = "grey"
+    content1.style.display = "block"
+}
+
+button1.onmouseout = function(event) {
+    event.preventDefault()
+
+    button1.style.backgroundColor = "#04abb4"
+    content1.style.display = "none"
+}
+
+content1.onmouseover = function(event) {
+    event.preventDefault()
+
+    button1.style.backgroundColor = "red"
+    content1.style.backgroundColor = "grey"
+    content1.style.display = "block"
+}
+
+content1.onmouseout = function(event) {
+    event.preventDefault()
+
+    button1.style.backgroundColor = "#04abb4"
+    content1.style.display = "none"
+}
+//end of button 1
+
+//button 2
+button2.onmouseover = function(event) {
+    event.preventDefault()
+
+    button2.style.backgroundColor = "red"
+    content2.style.backgroundColor = "grey"
+    content2.style.display = "block"
+}
+
+button2.onmouseout = function(event) {
+    event.preventDefault()
+
+    button2.style.backgroundColor = "#04abb4"
+    content2.style.display = "none"
+}
+
+content2.onmouseover = function(event) {
+    event.preventDefault()
+
+    button2.style.backgroundColor = "red"
+    content2.style.backgroundColor = "grey"
+    content2.style.display = "block"
+}
+
+content2.onmouseout = function(event) {
+    event.preventDefault()
+
+    button2.style.backgroundColor = "#04abb4"
+    content2.style.display = "none"
+}
+//end of button 2
